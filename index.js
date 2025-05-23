9d2bcf714452ed704e48b83397a4632ad7871393
bblair321 <brady@BradysPC.>
2025-05-22 21:44:45 -0700

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+async function fetchPost() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await response.json();
+
+  const h1 = document.createElement('h1');
+  h1.textContent = data.title;
+
+  const p = document.createElement('p');
+  p.textContent = data.body;
+
+  const postList = document.querySelector('#post-list');
+  if (postList) {
+    postList.appendChild(h1);
+    postList.appendChild(p);
+  }
+}
+
+fetchPost();
